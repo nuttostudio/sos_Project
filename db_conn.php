e55f22f2b549f63f65ba99afa997f68489d62b1c
kodchakorn <max.khamtan@gmail.com>
2018-02-22 10:17:19 +0700

    รับไป

--- db_conn.php
+++ db_conn.php
@@ -3,20 +3,25 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
+	$dbname = "sos";
 
 	// Create connection
-	$conn = new mysqli($servername, $username, $password);
+	$conn = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
 	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	} 
-
-	// Create database
-	$sql = "CREATE DATABASE myDB";
-	if ($conn->query($sql) === TRUE) {
-	    echo "Database created successfully";
-	} else {
-	    echo "Error creating database: " . $conn->error;
+	    // Create database
+		$sql = "CREATE DATABASE sos";
+		$conn->query($sql);
+	}else{
+		// sql to create table
+		$sql = "CREATE TABLE area (
+		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+		date_area VARCHAR(30),
+		time_area VARCHAR(30),
+		area_id VARCHAR(50),
+		status VARCHAR(50),
+		s_time TIMESTAMP
+		)";
+		$conn->query($sql);
 	}
-
 ?>
--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 	</head>
 	<body>
 		<?php
+			require 'db_conn.php';
 			include 'menu.php';
 			include 'home.php';
 		?>
